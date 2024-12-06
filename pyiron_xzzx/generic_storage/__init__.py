ce31ba8894413bd43f68d70007f2f786974cb1ae
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2024-12-06 14:24:28 +0100

    generic storage implementation

---
+++ pyiron_xzzx/generic_storage/__init__.py
@@ -0,0 +1,12 @@
+from pyiron_xzzx.generic_storage.interface import GenericStorage, StorageGroup
+from pyiron_xzzx.generic_storage.hdf5_storage import HDF5Group, HDF5Storage
+from pyiron_xzzx.generic_storage.json_storage import JSONGroup, JSONStorage
+
+__all__ = [
+    "GenericStorage",
+    "StorageGroup",
+    "HDF5Group",
+    "HDF5Storage",
+    "JSONGroup",
+    "JSONStorage",
+]
---
+++ pyiron_xzzx/generic_storage/dataclass_helpers.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from _typeshed import DataclassInstance
+
+from dataclasses import fields
+from pyiron_workflow.generic_storage.interface import StorageGroup
+
+
+def unwrap_dataclass(storage_group: StorageGroup, dataclass: DataclassInstance):
+    for field in fields(dataclass):
+        storage_group[field.name] = getattr(dataclass, field.name)
