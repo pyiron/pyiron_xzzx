dcf756f54e024bcfd274aa9879a6dbf473d0597b
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2025-02-14 14:56:52 +0100

    skip database tests if database is not there

--- tests/integration/test_postgresql.py
+++ tests/integration/test_postgresql.py
@@ -19,33 +19,39 @@ def AddNode(x: int = 1, y: int = 2) -> tuple[int, int]:
 
 
 class TestPostgreSQL(unittest.TestCase):
+    db: PostgreSQLInstanceDatabase
+
+    def setUp(self):
+        try:
+            self.db = PostgreSQLInstanceDatabase(
+                "postgresql://pyiron:pyiron@postgres/pyiron_workflow"
+            )
+        except ImportError as err:
+            raise unittest.SkipTest("Skipping due to import error.") from err
+
     def test_idempotence_of_drop_init(self):
-        db = PostgreSQLInstanceDatabase(
-            "postgresql://pyiron:pyiron@postgres/pyiron_workflow"
-        )
-        db.drop()
-        db.drop()
-        db.init()
-        db.init()
+        self.db.drop()
+        self.db.drop()
+        self.db.init()
+        self.db.init()
 
     def test_node_store_restore(self) -> None:
         node = AddNode(3, 4)
 
-        db = PostgreSQLInstanceDatabase(
-            "postgresql://pyiron:pyiron@postgres/pyiron_workflow"
-        )
-        db.drop()
-        db.init()
+        self.db.drop()
+        self.db.init()
 
         hash = store_node_in_database(
-            db, node, store_outputs=False, store_input_nodes_recursively=True
+            self.db, node, store_outputs=False, store_input_nodes_recursively=True
         )
 
-        node_restored = restore_node_from_database(db, hash)
+        node_restored = restore_node_from_database(self.db, hash)
         node_restored.run()
         self.assertEqual(node_restored.outputs.a.value, 7)
         self.assertEqual(node_restored.outputs.b.value, -1)
 
+
+class TestOutputStorage(unittest.TestCase):
     def test_node_store_restore_outputs(self) -> None:
         node_to_store = AddNode(3, 4)
         node_to_store.run()
